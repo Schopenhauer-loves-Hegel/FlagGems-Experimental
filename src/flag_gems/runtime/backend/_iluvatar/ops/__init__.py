db8e3b75ccff8897ac0de96c7749928dc8910ff1
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 14:54:16 +0800

    [KernelGen][Iluvatar] Add linalg_svdvals Iluvatar specialized operator (#377)
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -42,6 +42,7 @@ from .linalg_solve_triangular import (
     linalg_solve_triangular,
     linalg_solve_triangular_out,
 )
+from .linalg_svdvals import linalg_svdvals
 from .linear import linear
 from .log_normal import log_normal
 from .log_normal_ import log_normal_
@@ -112,6 +113,7 @@ __all__ = [
     "linalg_qr_out",
     "linalg_solve_triangular",
     "linalg_solve_triangular_out",
+    "linalg_svdvals",
     "linear",
     "log_normal",
     "log_normal_",
