2d09b794b43f633f953cadb4449ef3965bb4fea0
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-09 15:55:04 +0800

    [KernelGen][MThreads] Add lcm_ Moore Threads specialized operator (#436)
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -59,6 +59,7 @@ from .index_copy_ import index_copy, index_copy_
 from .index_put import _index_put_impl_, index_put, index_put_
 from .index_select import index_select
 from .kthvalue import kthvalue
+from .lcm_ import lcm_
 from .lift_out import lift_out
 from .linalg_cholesky import linalg_cholesky
 from .linalg_ldl_factor_ex import ldl_factor_ex
@@ -173,6 +174,7 @@ __all__ = [
     "_index_put_impl_",
     "index_select",
     "kthvalue",
+    "lcm_",
     "ldl_factor_ex",
     "lift_out",
     "linalg_cholesky",
