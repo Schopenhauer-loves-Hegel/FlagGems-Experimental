047c83378b8de88fc368e7633047c87188f8c54c
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-09 10:59:51 +0800

    [KernelGen][MThreads] Add special_round_out Moore Threads specialized operator (#448)

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -102,6 +102,7 @@ from .softplus_backward import softplus_backward
 from .sort import sort, sort_stable
 from .special_erfcx import special_erfcx
 from .special_gammainc import special_gammainc
+from .special_round_out import special_round_out
 from .tile import tile
 from .trunc import trunc, trunc_
 from .unique import _unique2
@@ -217,6 +218,7 @@ __all__ = [
     "sort_stable",
     "special_erfcx",
     "special_gammainc",
+    "special_round_out",
     "tile",
     "true_divide",
     "true_divide_",
