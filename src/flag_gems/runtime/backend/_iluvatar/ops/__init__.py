0042859e08e9d914447511314971632e42ac0c2b
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 11:37:47 +0800

    [KernelGen][Iluvatar] Add mvlgamma Iluvatar specialized operator (#380)
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -47,6 +47,7 @@ from .log_normal_ import log_normal_
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
 from .mm import mm, mm_out
+from .mvlgamma import run as mvlgamma
 from .narrow_copy import narrow_copy
 from .nonzero_numpy import nonzero_numpy
 from .permute_copy import permute_copy
@@ -115,6 +116,7 @@ __all__ = [
     "matmul_int8",
     "mm",
     "mm_out",
+    "mvlgamma",
     "narrow_copy",
     "nonzero_numpy",
     "permute_copy",
